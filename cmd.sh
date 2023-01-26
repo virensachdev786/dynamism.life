05458f50d9e0bc37e55c469698dd050b685e1813
Viren Sachdev <viren.sachdev2@encora.com>
2023-01-26 13:56:00 -0500

    updated run script to run vm_one

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,10 @@ docker-compose up -d
 # List running containers
 docker ps
 
-# Go inside the container
-docker exec -it $[container_name] /bin/bash
-:
+# Go inside the container vm_one
+docker exec -it vm_one /bin/bash
+
+# Go inside the container vm_two
+#docker exec -it vm_two /bin/bash
+
+
