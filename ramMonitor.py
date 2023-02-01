e3bf6bd3dd75c7da72e4ec4541d7943df6728af5
andyp2001 <andypacheco@mail.adelphi.edu>
2023-02-01 16:21:57 -0500

    ram monitor

---
+++ ramMonitor.py
@@ -0,0 +1,23 @@
+# Program to compute both the CPU and Ram
+
+
+import time
+
+import psutil
+
+
+def display_usage(cpu_usage, mem_usage, bars= 50):
+        cpu_percent = (cpu_usage / 100.0)
+        cpu_bar = '|' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+
+        mem_percent = (mem_usage / 100.0)
+        mem_bar = '|' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
+
+        print(f"\rCPU Usage: |{cpu_bar}| {cpu_usage:.2f}% ", end="")
+        print(f"RAM Usage: |{mem_bar}| {mem_usage:.2f}% ", end="\r")
+
+while True:
+        display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+        time.sleep(0.5)
+
+
